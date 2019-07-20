72b341404d4a363c9ffdb78e70b655d9cfde8a75
adam <dynamicsydney@gmail.com>
2019-07-20 16:23:04 +1000

    Revert "Revert "added basecake edit form""
    This reverts commit ab0eb0c968021dcac591d2ef9695bb792e914103.

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,14 +1,49 @@
 import React, { Component } from "react";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { logoutAuthToken } from "../../actions";
+import { connect } from "react-redux";
 
 class HomePage extends Component {
     render() {
         return (
-            <div>
-                <h1>Welcome To Cake blah!</h1>
+            <>
+            <div className="ui right aligned grid">
+                <div className="center aligned two column row">
+                    <div className="column">
+                        <Link to="/ingredients">
+                            <div className="ui pink segment">
+                                Add an ingredient
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="column">
+                        <Link to="/baseCakes">
+                            <div className="ui pink segment">
+                                Add a base cake recipe
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+                <div className="center aligned two column row">
+                    <div className="column">
+                        <Link to="/#">
+                            <div className="ui pink segment">
+                                Create new orders
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="column">
+                        <Link to="/#">
+                            <div className="ui pink segment">
+                                Current and past orders
+                            </div>
+                        </Link>
+                    </div>
+                </div>
             </div>
+        </>
         );
     }
 }
 
-export default HomePage;
+export default connect(null, {logoutAuthToken})(HomePage);
--- src/components/pages/RegisterPage.js
+++ src/components/pages/RegisterPage.js
@@ -5,7 +5,8 @@ class RegisterPage extends Component {
     render() {
         return(
             <div>
-                <h1>Register a new user</h1>
+                <h1 className="hometitle">Cake Calculator</h1>
+                <h2>Register</h2>
                 <RegisterForm {...this.props} />
             </div>
         );
